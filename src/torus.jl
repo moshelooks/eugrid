e553067d9ebbf0271432feda074bdc6370c9376b
Moshe Looks <moshe.looks@gmail.com>
2021-06-09 20:54:20 -0700

    wip

--- src/torus.jl
+++ src/torus.jl
@@ -9,7 +9,6 @@ struct Torus
     sed::Matrix{Int}
     dk::Array{Int, 3}
     dm::Array{Int, 3}
-    scores::Matrix{Int}
     diags::CircularArray{Bool, 2, BitMatrix}
 end
 
@@ -21,27 +20,33 @@ function Torus(n::Int, k::Int)
     sed = ls .+ reshape(reverse(ls) .+ [onexy], 1, :) .|> x->sum(x.I.^2)
     dk = fill(2*k, (m, n, n))
     dm = fill(2*m, (w, n, n))
-    scores = Matrix{Int}(undef, n, n)
-    t = Torus(n, k, m, w, ls, sed, dk, dm, scores, CircularArray(falses(n, n)))
-    fill!(scores, score(t, onexy))
-    t
+    Torus(n, k, m, w, ls, sed, dk, dm, CircularArray(falses(n, n)))
 end
 
-wrap(t::Torus, i::CartesianIndex{2}) = CartesianIndex(mod.(i.I, axes(t.scores)))
+wrap(t::Torus, i::CartesianIndex{2}) = CartesianIndex(mod.(i.I, axes(t.diags)))
 
-function score(t::Torus, u::CartesianIndex{2})::Int
-    t.diags[u] && return 0
+function score(t::Torus, u::CartesianIndex{2})
+    if t.diags[u]
+        return 0
+        t2 = Torus(t.n, t.k)
+        for v in Staircase(t, u)
+            if t.diags[v] && v != u
+                add_diag(t2, v)
+            end
+        end
+        return -score(t2, u)
+    end
 
     ls = t.ls .|> l->wrap(t, u - l)
     dlu = t.dk[CartesianIndex.(t.m:-1:1, ls)]
     dur = 1 .+ view(t.dk, :, wrap(t, u + onexy))
 
-    s = 0.0
+    s = 0
     for j in eachindex(dur), i in eachindex(dlu)
         diuj = dlu[i] + dur[j]
         dij = t.dm[t.m-i+j, ls[i]]
         if diuj < dij
-            s += 3 * (diuj * dij - t.sed[i, j]) + 1
+            s += (3 * (diuj * dij - t.sed[i, j]) + 1)
             #s += 2 * (dij - sqrt(t.sed[i,j])) - 1
         end
     end
@@ -102,53 +107,87 @@ function add_diag(t::Torus, u::CartesianIndex{2})::Nothing
         dv = view(t.dk, i[2]:t.m-i[1]+1, v)
         dv .= min(dv, dvu .+ antidiag(du, 1 - t.k - sum(i.I)))
     end
-
-    for v in Staircase(t, u)
-        t.scores[v] = score(t, v)
-    end
 end
 
-function add_best(t::Torus)
-    s, u = findmax(t.scores)
+function remove_diag(t::Torus, u::CartesianIndex{2})::Nothing
+    @assert t.diags[u]
+    t.diags[u] = false
+
+    t2 = Torus(t.n, t.k)
+    for v in CartesianIndices(t.diags)
+        add_diag(t2, v)
+    end
+    copy!(t.dk, t2.dk)
+    copy!(t.dm, t2.dm)
+    nothing
+end
+
+
+function add_best(t::Torus, scores::AbstractMatrix)
+    d = density(t)
+    (s, _), u = findmax(collect(zip(scores, .-d)))
+    #s, u = findmax(scores)
     s <= 0 && return nothing
-    add_diag(t, u)
+    if !t.diags[u]
+        add_diag(t, u)
+    else
+        remove_diag(t, u)
+    end
+    for v in Staircase(t, u)
+        scores[v] = score(t, v)
+    end
     (s, u)
 end
 
-function add_all(t::Torus)
+function add_all(t::Torus, scores::AbstractMatrix)
     for i in 1:length(t.diags)
-        x = add_best(t)
+        x = add_best(t, scores)
         #t = foo(t)
         x == nothing && return
-        #println(i, " ", x)
+        println(i, " ", x, " ", t.diags[x[2]])
     end
 end
 
 function err(t::Torus)
     de = [sqrt(i^2+(t.w+1-i)^2) for i in 1:t.w]
-    (t.dm .- de) ./ de
+    (t.dm .- de)
 end
 
+function density(t::Torus)
+    d = CircularArray(convert.(Float64, t.diags))
+    for _ in 1:t.m
+        for i in CartesianIndices(t.diags)
+            #dlocal = (sum(d[j] for j in CartesianIndices(i-onexy:i+onexy)) - d[i]) / 8
+            dlocal = (d[i - onexy] + d[i + onexy]) / 2
+            d[i] = 0.9 * d[i] + 0.1 * dlocal
+        end
+    end
+    d
+end
+
+
 function expand(t::Torus)
-    big = Torus(2*t.n, div(2*t.n - 1, 4))
+    #big = Torus(2*t.n, div(2*t.n - 1, 4))
+    big = Torus(t.w+4, t.k+1)
     for i in CartesianIndices(t.diags)
         if t.diags[i]
-            add_diag(big, 2 * i)
+            add_diag(big, i)
+            #add_diag(big, 2 * i)
         end
     end
     big
 end
 
-
-function grow(k)
-    t = Torus(1, 0)
-    add_all(t)
+function grow(k, t = Torus(1, 0))
+    scores = fill(score(t, onexy), t.n, t.n)
+    add_all(t, scores)
     println(maximum(abs.(err(t))))
     #t = foo(t)
     for i in 2:k
         t = expand(t)
         println("n=",t.n)
-        add_all(t)
+        scores = [score(t, i) for i in CartesianIndices(t.diags)]
+        add_all(t, scores)
         println(maximum(abs.(err(t))), " ", mean(abs.(err(t))))
         #t = foo(t)
     end
